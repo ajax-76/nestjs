5bcfd4642638e744367dbfd9f2981c22fbb98c48
Ankit Seth <cosmic.ankit@gmail.com>
2024-07-15 15:55:26 +0530

    logger added

--- api-getway-new/src/middleware/request.middleware.ts
+++ api-getway-new/src/middleware/request.middleware.ts
@@ -11,9 +11,15 @@ export class RequestValidationMiddleware  implements NestMiddleware{
         console.log(req.query);
         const accessKey=req.query.accessKey;
         let userObjectAccessPlan: UserViewModel;
-        userObjectAccessPlan= await lastValueFrom(
+        try{
+          userObjectAccessPlan= await lastValueFrom(
             this.communicationProxy.send({ cmd: 'fetch-access-plan' },
             { accessKey}));
+        }
+        catch(err){
+          throw new HttpException('access key do not exist',HttpStatus.NOT_FOUND) 
+        }
+
         console.log(`user intercept ${JSON.stringify(userObjectAccessPlan)}`)
         // can add on throttler here 
         this.ValidationCheck(userObjectAccessPlan)
@@ -21,23 +27,36 @@ export class RequestValidationMiddleware  implements NestMiddleware{
         const throttleKey =`user:${userObjectAccessPlan.accessKey}`
         console.log('Checking the request rate')
         if (await this.tokenService.TokenBucketTakeToken(throttleKey,10,limit)) {
+          // loggging the success
+           this.logger(true,accessKey,'request successfully processed')
           next();
         } else {
+            this.logger(false,accessKey,'request is rate limited')
             console.log('throttle exception')
             throw new HttpException('too many requests',HttpStatus.BAD_REQUEST);
         }
     }
 
+     private logger(success:boolean,accessKey:string,message:string){
+      console.log(`${message }: ${JSON.stringify({
+        accessKey:accessKey,
+        timeStamp:new Date().toLocaleDateString(),
+        success:success,
+      })}`)
+     }
      private ValidationCheck(userObjectAccessPlan?: UserViewModel){
         if(userObjectAccessPlan===undefined){
+        
             throw new HttpException('access key do not exist',HttpStatus.NOT_FOUND) 
         }
         if(userObjectAccessPlan.isEnable!==undefined && userObjectAccessPlan.isEnable===false){
+         
           throw new HttpException('access key is disbaled',HttpStatus.BAD_REQUEST) 
         }
         const expirationTime = new Date(userObjectAccessPlan.expirationTime)
         console.log(`expirationTime : ${expirationTime} : ${Date.now()}`)
         if(expirationTime!==undefined && this.isExpired(expirationTime.getTime())){
+  
             throw new HttpException('access key is expired',HttpStatus.BAD_REQUEST) 
         }
       }
